bb017b80df037a94cfcc05ec8659b61851ce9e59
Chris Kampmeier <chris@cfcarts.com>
2019-11-11 18:26:23 -0500

    Refactor remaining tests and naming
    Now that alternate domains are only handled one way, we can simplify things.

--- test/test_domains.rb
+++ test/test_domains.rb
@@ -1,14 +1,12 @@
 require 'test_helper'
 
 class TestDomains < Test::Unit::TestCase
-  ALT_DOMAINS_WITH_DNS_MANAGED_BY_REGISTRAR = %w(cfcommunityarts.com
-                                                 centralfloridacommunityarts.com
-                                                 centralfloridacommunityarts.org
-                                                 cfcommunitychoir.com
-                                                 cfcarts.net
-                                                 cfcarts.org)
-
-  ALL_ALT_DOMAINS = ALT_DOMAINS_WITH_DNS_MANAGED_BY_REGISTRAR
+  ALT_DOMAINS = %w(cfcommunityarts.com
+                   centralfloridacommunityarts.com
+                   centralfloridacommunityarts.org
+                   cfcommunitychoir.com
+                   cfcarts.net
+                   cfcarts.org)
 
   MAIL_DOMAINS = %w(cfcarts.com cfcommunityarts.com)
 
@@ -29,8 +27,8 @@ class TestDomains < Test::Unit::TestCase
     end
   end
 
-  def test_all_alternate_domains_redirect_http_using_301
-    domains = with_wwws(ALL_ALT_DOMAINS)
+  def test_alternate_domains_redirect_http_using_301
+    domains = with_wwws(ALT_DOMAINS)
     domains.each do |d|
       uri = URI("http://#{d}")
       response = Net::HTTP.get_response(uri)
@@ -40,9 +38,9 @@ class TestDomains < Test::Unit::TestCase
     end
   end
 
-  def test_registrar_alternate_domains_do_not_listen_on_443
+  def test_alternate_domains_do_not_listen_on_443
     # Not intended behavior but this is how it works right now
-    domains = with_wwws(ALT_DOMAINS_WITH_DNS_MANAGED_BY_REGISTRAR)
+    domains = with_wwws(ALT_DOMAINS)
     domains.each do |d|
       uri = URI("https://#{d}")
       http = Net::HTTP.new(uri.host, uri.port)
@@ -55,9 +53,9 @@ class TestDomains < Test::Unit::TestCase
     end
   end
 
-  def test_registrar_alternate_domains_redirect_http_to_https_canonical_immediately
+  def test_alternate_domains_redirect_http_to_https_canonical_immediately
     # Intended behavior!
-    domains = with_wwws(ALT_DOMAINS_WITH_DNS_MANAGED_BY_REGISTRAR)
+    domains = with_wwws(ALT_DOMAINS)
     domains.each do |d|
       uri = URI("http://#{d}")
       redirected_to = last_location_after_following_redirects(uri, 1)
@@ -68,7 +66,7 @@ class TestDomains < Test::Unit::TestCase
   end
 
   def test_alternate_domains_do_not_have_mx_records
-    alt_domains_without_mx_records = ALL_ALT_DOMAINS - MAIL_DOMAINS
+    alt_domains_without_mx_records = ALT_DOMAINS - MAIL_DOMAINS
     Resolv::DNS.open do |dns|
       alt_domains_without_mx_records.each do |d|
         records = dns.getresources(d, Resolv::DNS::Resource::IN::MX)
