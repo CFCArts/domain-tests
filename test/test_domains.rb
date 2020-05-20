fc8a7bdbb892add9873188d05a1f74aa1c141bc2
Chris Kampmeier <chris@cfcarts.com>
2020-05-20 00:59:59 -0400

    Add comment about known intermittent failure

--- test/test_domains.rb
+++ test/test_domains.rb
@@ -149,6 +149,8 @@ class TestDomains < Test::Unit::TestCase
 
   def test_mail_domains_mx_records_have_sane_ttls
     # Google suggests 1 hour; longer seems appropriate
+    # TODO: investigate new intermittent failure: Namecheap seems to be
+    # responding with TTLs with a jitter of up to 20% or so.
     sanity_cutoff_in_sec = 60 * 60
     Resolv::DNS.open do |dns|
       MAIL_DOMAINS.each do |d|
