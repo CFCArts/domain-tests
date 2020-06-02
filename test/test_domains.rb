93ff5c57c52c3990ed1e304a0d7fe05d854fe5ca
Chris Kampmeier <chris@cfcarts.com>
2020-06-02 12:52:23 -0400

    Add test for new redirect

--- test/test_domains.rb
+++ test/test_domains.rb
@@ -225,4 +225,15 @@ class TestDomains < Test::Unit::TestCase
       end
     end
   end
+
+  def test_pm_shortcut_redirects_to_patronmanager_using_301
+    uri = URI("http://pm.cfcarts.com")
+    response = Net::HTTP.get_response(uri)
+    assert_equal "https://patronmanager.cloudforce.com",
+                 response["location"],
+                 "#{uri} redirected to the wrong place"
+    assert_equal "301 Moved Permanently",
+                 "#{response.code} #{response.message}",
+                 "#{uri} had the wrong response code"
+  end
 end
