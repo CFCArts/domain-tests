2e768c09fef251aa6744dc3120a0d8292fb87022
Chris Kampmeier <chris@cfcarts.com>
2020-08-08 18:34:25 -0400

    Move helpers to a module for reuse

--- test/test_domains.rb
+++ test/test_domains.rb
@@ -1,31 +1,7 @@
 require 'test_helper'
 
 class TestDomains < Test::Unit::TestCase
-  ALT_DOMAINS = %w(cfcommunityarts.com
-                   centralfloridacommunityarts.com
-                   centralfloridacommunityarts.org
-                   cfcommunitychoir.com
-                   cfcarts.net
-                   cfcarts.org)
-
-  MAIL_DOMAINS = %w(cfcarts.com cfcommunityarts.com)
-
-  CANONICAL_URI = URI("https://cfcarts.com")
-
-  def with_wwws(domain_list)
-    domain_list + domain_list.map { |d| "www.#{d}" }
-  end
-
-  def last_location_after_following_redirects(uri, limit = 5)
-    raise 'Too many redirects' if limit < 0
-
-    response = Net::HTTP.get_response(uri)
-    if response.is_a?(Net::HTTPRedirection)
-      last_location_after_following_redirects(URI(response["location"]), limit - 1)
-    else
-      uri
-    end
-  end
+  include TestHelpers
 
   def test_alternate_domains_redirect_http_using_301
     domains = with_wwws(ALT_DOMAINS)
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,3 +6,31 @@ require 'test/unit'
 require 'net/http'
 require 'resolv'
 require 'amazing_print'
+
+module TestHelpers
+  ALT_DOMAINS = %w(cfcommunityarts.com
+                   centralfloridacommunityarts.com
+                   centralfloridacommunityarts.org
+                   cfcommunitychoir.com
+                   cfcarts.net
+                   cfcarts.org)
+
+  MAIL_DOMAINS = %w(cfcarts.com cfcommunityarts.com)
+
+  CANONICAL_URI = URI("https://cfcarts.com")
+
+  def with_wwws(domain_list)
+    domain_list + domain_list.map { |d| "www.#{d}" }
+  end
+
+  def last_location_after_following_redirects(uri, limit = 5)
+    raise 'Too many redirects' if limit < 0
+
+    response = Net::HTTP.get_response(uri)
+    if response.is_a?(Net::HTTPRedirection)
+      last_location_after_following_redirects(URI(response["location"]), limit - 1)
+    else
+      uri
+    end
+  end
+end
